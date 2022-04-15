f84d70f0a8bc931643c50f0161866c876eb4a4c2
blockjjam <blockjjam99@gmail.com>
2022-04-15 21:01:27 +0900

    feat: 프록시 패턴 테스트 코드 작성 - 캐싱

---
+++ src/test/java/hello/proxy/pureproxy/proxy/code/CacheProxy.java
@@ -0,0 +1,25 @@
+package hello.proxy.pureproxy.proxy.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class CacheProxy implements Subject{
+
+    // 프록시 입장에서 호출해야 하는 대상
+    private Subject target;
+    private String cacheValue;
+
+    public CacheProxy(Subject target) {
+        this.target = target;
+    }
+
+    @Override
+    public String operation() {
+        log.info("프록시 호출");
+        if(cacheValue == null){
+            cacheValue = target.operation();
+        }
+
+        return cacheValue;
+    }
+}
