41f0c11dd28b57fcb5f493c4215c04a445aa0c45
blockjjam <blockjjam99@gmail.com>
2022-04-22 16:45:53 +0900

    feat: AspectJExpressionPointcut을 활용해, 특정 하위 패키지의 빈등록에만 포인트컷을 적용하는 표현식 활용 및 특정 메서드 배제하는 표현식까지 테스트

--- src/main/java/hello/proxy/config/v5_autoproxy/AutoProxyConfig.java
+++ src/main/java/hello/proxy/config/v5_autoproxy/AutoProxyConfig.java
@@ -6,6 +6,7 @@ import hello.proxy.config.v3_proxyfactory.advice.LogTraceAdvice;
 import hello.proxy.trace.logtrace.LogTrace;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.aop.support.NameMatchMethodPointcut;
 import org.springframework.context.annotation.Bean;
@@ -19,11 +20,23 @@ public class AutoProxyConfig {
 
     // advisor만 등록하면 끝난다, 스프링이 자동으로 자동 프록시 생성기라는 BeanPostProccesor는 것을 등록해놓는다
     // 찾아올 advisor만 만들어 놓으면 끝
-    @Bean
+ //   @Bean
     public Advisor advisor1(LogTrace logTrace) {
         // pointcut
         NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
         pointcut.setMappedNames("request*", "order*", "save*");
+
+        // advice
+        LogTraceAdvice advice = new LogTraceAdvice(logTrace);
+        return new DefaultPointcutAdvisor(pointcut, advice);
+    }
+
+
+    @Bean
+    public Advisor advisor2(LogTrace logTrace){
+        // pointcut
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("execution(* hello.proxy.app..*(..)) && !execution(* hello.proxy.app..noLog(..))"); // 해당 패키지 위치에만 빈등록에 프록시 생성을 관여한다
 
         // advice
         LogTraceAdvice advice = new LogTraceAdvice(logTrace);
