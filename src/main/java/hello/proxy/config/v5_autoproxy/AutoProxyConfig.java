cd09eeb83ab15fef1999d07107e2b8a265cb0db3
blockjjam <blockjjam99@gmail.com>
2022-04-22 16:03:20 +0900

    feat: AOP 의존성을 연결하고, 자동 프록시 생성기를 통해 어드바이저를 이용한 자동 프록시 생성을 테스트

---
+++ src/main/java/hello/proxy/config/v5_autoproxy/AutoProxyConfig.java
@@ -0,0 +1,32 @@
+package hello.proxy.config.v5_autoproxy;
+
+import hello.proxy.config.AppV1Config;
+import hello.proxy.config.AppV2Config;
+import hello.proxy.config.v3_proxyfactory.advice.LogTraceAdvice;
+import hello.proxy.trace.logtrace.LogTrace;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.Advisor;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Slf4j
+@Configuration
+@Import({AppV1Config.class, AppV2Config.class})
+public class AutoProxyConfig {
+
+    // advisor만 등록하면 끝난다, 스프링이 자동으로 자동 프록시 생성기라는 BeanPostProccesor는 것을 등록해놓는다
+    // 찾아올 advisor만 만들어 놓으면 끝
+    @Bean
+    public Advisor advisor1(LogTrace logTrace) {
+        // pointcut
+        NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
+        pointcut.setMappedNames("request*", "order*", "save*");
+
+        // advice
+        LogTraceAdvice advice = new LogTraceAdvice(logTrace);
+        return new DefaultPointcutAdvisor(pointcut, advice);
+    }
+}
